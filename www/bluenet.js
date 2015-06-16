f02e13d337aa7cba117d36b3ff964541c02ac303
Vliedel <bart@dobots.nl>
2015-06-16 16:54:09 +0200

    just export to window for now..

--- www/bluenet.js
+++ www/bluenet.js
@@ -1797,10 +1797,27 @@ var BleExt = (function () {
     return BleExt;
 })();
 /// <reference path="ble-ext.ts"/>
-var exports = module.exports = {};
-Object.defineProperty(exports, "BleBase", BleBase);
-Object.defineProperty(exports, "BleState", BleState);
-Object.defineProperty(exports, "BleFilter", BleFilter);
-Object.defineProperty(exports, "BleDevice", BleDevice);
-Object.defineProperty(exports, "BleExt", BleExt);
-Object.defineProperty(exports, "BleUtils", BleUtils);
+//var exports = module.exports = {};
+//Object.defineProperty(exports, "BleBase", BleBase);
+//Object.defineProperty(exports, "BleState", BleState);
+//Object.defineProperty(exports, "BleFilter",BleFilter);
+//Object.defineProperty(exports, "BleDevice", BleDevice);
+//Object.defineProperty(exports, "BleExt", BleExt);
+//Object.defineProperty(exports, "BleUtils", BleUtils);
+var bluenet = {};
+bluenet.BleBase = BleBase;
+bluenet.BleState = BleState;
+bluenet.BleDevice = BleDevice;
+bluenet.BleExt = BleExt;
+bluenet.BleUtils = BleUtils;
+bluenet.BleFilter = BleFilter;
+if (window) {
+    console.log("export to window");
+    window.BleBase = BleBase;
+    window.BleState = BleState;
+    window.BleDevice = BleDevice;
+    window.BleExt = BleExt;
+    window.BleUtils = BleUtils;
+    window.BleFilter = BleFilter;
+}
+module.exports = bluenet;
